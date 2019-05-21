2d7e58d93113e2abc9b214ce43a7b5a1acd26e44
Buder-Joker <2543363495@qq.com>
2019-05-21 17:08:48 +0800

    sre2往str1里边插，判断新的字符串是否为回文，若是，count+1

---
+++ 牛客网题/插入字符串判断回文个数/Demo1.java
@@ -0,0 +1,58 @@
+package NiuKeWang;
+/*
+str1 = aba;
+str2 = b;
+sre2往str1里边插，判断新的字符串是否为回文，若是 count+1；
+ */
+import java.util.Scanner;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String str1 = in.next();
+        //System.out.println(isHuiWen(str1));
+        String str2 = in.next();
+        //System.out.println(isHuiWen(str2));
+
+        StringBuilder sb1 = new StringBuilder(str1);
+        StringBuilder sb2 = new StringBuilder(str2);
+        int count = 0;
+        char[] arr = str1.toCharArray();
+        int lenth = arr.length;
+        String stringBuilder = sb1+"";
+        for(int i=0; i<lenth; i++){
+            StringBuilder sb = sb1.insert(i,sb2);
+            System.out.println(sb);
+            String str = sb+"";
+            if(isHuiWen(str)){
+                count++;
+            }
+            sb1 = new StringBuilder(stringBuilder);
+        }
+        System.out.println(count);
+
+    }
+    public static boolean isHuiWen(String str){
+        char[] arr = str.toCharArray();
+        char[] srr = new char[arr.length/2];
+        for(int i=0; i<srr.length; i++){
+            srr[i] = arr[i];
+        }
+        int index = 0;
+        int t = srr.length-1;
+        if(arr.length%2==0){
+            index = arr.length/2;
+        }else {
+            index = arr.length/2+1;
+        }
+        for(int i =index; i<arr.length; i++){
+            if(arr[i]==srr[t]){
+                if(t==0){
+                    return true;
+                }
+                t--;
+            }
+        }
+        return false;
+    }
+}
