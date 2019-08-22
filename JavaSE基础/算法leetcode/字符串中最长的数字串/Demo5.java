ae5f24c2b6325819a4bb7f76f289baf41dab9187
Buder-Joker <2543363495@qq.com>
2019-08-22 22:45:41 +0800

    字符串中最长的数字串/Demo5.java

---
+++ JavaSE基础/算法leetcode/字符串中最长的数字串/Demo5.java
@@ -0,0 +1,29 @@
+package 算法;
+
+import java.util.Scanner;
+
+/*
+输出在字符串中最长数字串
+ */
+public class Demo5 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+        System.out.println(longString(str));
+    }
+    private static String longString(String str) {
+        String ans = "";
+        String ret = "";
+        char[] arr = str.toCharArray();
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]>='0'&&arr[i]<='9'){
+                ans+=arr[i];
+                ret=ret.length()>ans.length()?ret:ans;
+            }else{
+                ans="";
+            }
+        }
+        ans = ans.length()>ret.length()?ans:ret;
+        return ans;
+    }
+}
